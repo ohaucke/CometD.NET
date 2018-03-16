a596aa42cff6324237f9b61ac37c34682975519f
Oliver Haucke <ohaucke@gadean.de>
2018-03-16 17:36:28 +0100

    build script update

--- build.bat
+++ build.bat
@@ -5,8 +5,7 @@ set PATHTOFILE=bin\Release\cometd.dll
 
 msbuild %NAME%.sln /t:rebuild /p:Configuration=Release
 FOR /F "tokens=*" %%a in ('AssemblyVersion.exe "%PATHTOFILE%"') do SET VERSION=%%a
-set VERSION=%VERSION:~0,5%
-echo %VERSION%
+
 if "%SIGN%" == "true" (
     call "D:\workspace\dev\ShellScripte\CodeSigning.bat" "%PATHTOFILE%"
 )
@@ -14,3 +13,4 @@ if "%SIGN%" == "true" (
 nuget pack %NAME%.nuspec -Version %VERSION%
 nuget add %NAME%.%VERSION%.nupkg -Source \\samba.host.0x59.de\sonstiges\Nuget.Packages
 del %NAME%.%VERSION%.nupkg
+pause
